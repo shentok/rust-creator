7300c0af13477b24ec881e337a780f9ec4093d7e
Bernhard Beschow <shentey@gmail.com>
2019-11-10 20:38:05 +0100

    --code style

--- nim.pro
+++ nim.pro
@@ -12,45 +12,31 @@ INCLUDEPATH += $$PWD
 HEADERS += \
     nimplugin.h \
     nimconstants.h \
-    editor/nimindenter.h \
     project/nimblebuildconfiguration.h \
     project/nimblebuildstep.h \
     project/nimbleproject.h \
     project/nimblerunconfiguration.h \
     project/nimbletaskstep.h \
     project/nimoutputtaskparser.h \
-    tools/nimlexer.h \
-    tools/sourcecodestream.h \
     project/nimbuildsystem.h \
     project/nimblebuildsystem.h \
-    settings/nimcodestylesettingspage.h \
-    settings/nimcodestylepreferencesfactory.h \
     settings/nimsettings.h \
-    settings/nimcodestylepreferenceswidget.h \
     project/nimtoolchain.h \
     project/nimtoolchainfactory.h \
 
 SOURCES += \
     nimplugin.cpp \
-    editor/nimindenter.cpp \
     project/nimblebuildconfiguration.cpp \
     project/nimblebuildstep.cpp \
     project/nimbletaskstep.cpp \
     project/nimbleproject.cpp \
     project/nimblerunconfiguration.cpp \
     project/nimoutputtaskparser.cpp \
-    tools/nimlexer.cpp \
     project/nimbuildsystem.cpp \
     project/nimblebuildsystem.cpp \
-    settings/nimcodestylesettingspage.cpp \
-    settings/nimcodestylepreferencesfactory.cpp \
     settings/nimsettings.cpp \
-    settings/nimcodestylepreferenceswidget.cpp \
     project/nimtoolchain.cpp \
     project/nimtoolchainfactory.cpp \
-
-FORMS += \
-    settings/nimcodestylepreferenceswidget.ui \
 
 include(nim_dependencies.pri)
 
--- settings/nimsettings.cpp
+++ settings/nimsettings.cpp
@@ -24,90 +24,20 @@
 ****************************************************************************/
 
 #include "nimsettings.h"
-#include "nimcodestylepreferencesfactory.h"
 
 #include "../nimconstants.h"
 
-#include <texteditor/icodestylepreferencesfactory.h>
-#include <texteditor/texteditorsettings.h>
-#include <texteditor/codestylepool.h>
-#include <texteditor/simplecodestylepreferences.h>
-#include <texteditor/tabsettings.h>
 #include <coreplugin/icore.h>
 
-using namespace TextEditor;
-
 namespace Nim {
-
-static SimpleCodeStylePreferences *m_globalCodeStyle = nullptr;
 
 NimSettings::NimSettings(QObject *parent)
     : QObject(parent)
 {
-    InitializeCodeStyleSettings();
 }
 
 NimSettings::~NimSettings()
 {
-    TerminateCodeStyleSettings();
-}
-
-SimpleCodeStylePreferences *NimSettings::globalCodeStyle()
-{
-    return m_globalCodeStyle;
-}
-
-void NimSettings::InitializeCodeStyleSettings()
-{
-    // code style factory
-    auto factory = new NimCodeStylePreferencesFactory();
-    TextEditorSettings::registerCodeStyleFactory(factory);
-
-    // code style pool
-    auto pool = new CodeStylePool(factory, this);
-    TextEditorSettings::registerCodeStylePool(Nim::Constants::C_NIMLANGUAGE_ID, pool);
-
-    m_globalCodeStyle = new SimpleCodeStylePreferences();
-    m_globalCodeStyle->setDelegatingPool(pool);
-    m_globalCodeStyle->setDisplayName(tr("Global", "Settings"));
-    m_globalCodeStyle->setId(Nim::Constants::C_NIMGLOBALCODESTYLE_ID);
-    pool->addCodeStyle(m_globalCodeStyle);
-    TextEditorSettings::registerCodeStyle(Nim::Constants::C_NIMLANGUAGE_ID, m_globalCodeStyle);
-
-    auto nimCodeStyle = new SimpleCodeStylePreferences();
-    nimCodeStyle->setId("nim");
-    nimCodeStyle->setDisplayName(tr("Nim"));
-    nimCodeStyle->setReadOnly(true);
-
-    TabSettings nimTabSettings;
-    nimTabSettings.m_tabPolicy = TabSettings::SpacesOnlyTabPolicy;
-    nimTabSettings.m_tabSize = 2;
-    nimTabSettings.m_indentSize = 2;
-    nimTabSettings.m_continuationAlignBehavior = TabSettings::ContinuationAlignWithIndent;
-    nimCodeStyle->setTabSettings(nimTabSettings);
-
-    pool->addCodeStyle(nimCodeStyle);
-
-    m_globalCodeStyle->setCurrentDelegate(nimCodeStyle);
-
-    pool->loadCustomCodeStyles();
-
-    // load global settings (after built-in settings are added to the pool)
-    QSettings *s = Core::ICore::settings();
-    m_globalCodeStyle->fromSettings(QLatin1String(Nim::Constants::C_NIMLANGUAGE_ID), s);
-
-    TextEditorSettings::registerMimeTypeForLanguageId(Nim::Constants::C_NIM_MIMETYPE,
-                                                      Nim::Constants::C_NIMLANGUAGE_ID);
-}
-
-void NimSettings::TerminateCodeStyleSettings()
-{
-    TextEditorSettings::unregisterCodeStyle(Nim::Constants::C_NIMLANGUAGE_ID);
-    TextEditorSettings::unregisterCodeStylePool(Nim::Constants::C_NIMLANGUAGE_ID);
-    TextEditorSettings::unregisterCodeStyleFactory(Nim::Constants::C_NIMLANGUAGE_ID);
-
-    delete m_globalCodeStyle;
-    m_globalCodeStyle = nullptr;
 }
 
 } // namespace Nim
