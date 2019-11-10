859d46511f84016da11d601e3f42701a0c7b0e32
Bernhard Beschow <shentey@gmail.com>
2019-11-10 20:38:05 +0100

    --code style

--- nimplugin.cpp
+++ nimplugin.cpp
@@ -32,8 +32,6 @@
 #include "project/nimtoolchainfactory.h"
 #include "project/nimblebuildstep.h"
 #include "project/nimbletaskstep.h"
-#include "settings/nimcodestylepreferencesfactory.h"
-#include "settings/nimcodestylesettingspage.h"
 #include "settings/nimsettings.h"
 
 #include <coreplugin/fileiconprovider.h>
@@ -65,8 +63,6 @@ public:
     };
     NimbleBuildStepFactory nimbleBuildStepFactory;
     NimbleTaskStepFactory nimbleTaskStepFactory;
-    NimCodeStyleSettingsPage codeStyleSettingsPage;
-    NimCodeStylePreferencesFactory codeStylePreferencesPage;
     NimToolChainFactory toolChainFactory;
 };
 
