d5e5d7bcd2050c091cf6e8214db31e8b07f4b4db
Bernhard Beschow <shentey@gmail.com>
2019-11-10 20:34:37 +0100

    Remove NimSuggest support... rely on LSP plugin instead

--- nimplugin.cpp
+++ nimplugin.cpp
@@ -39,9 +39,7 @@
 #include "project/nimbletaskstep.h"
 #include "settings/nimcodestylepreferencesfactory.h"
 #include "settings/nimcodestylesettingspage.h"
-#include "settings/nimtoolssettingspage.h"
 #include "settings/nimsettings.h"
-#include "suggest/nimsuggestcache.h"
 
 #include <coreplugin/fileiconprovider.h>
 #include <projectexplorer/projectmanager.h>
@@ -56,15 +54,6 @@ namespace Nim {
 class NimPluginPrivate
 {
 public:
-    NimPluginPrivate()
-        : toolsSettingsPage(&settings)
-    {
-        Suggest::NimSuggestCache::instance().setExecutablePath(settings.nimSuggestPath());
-        QObject::connect(&settings, &NimSettings::nimSuggestPathChanged,
-                         &Suggest::NimSuggestCache::instance(),
-                         &Suggest::NimSuggestCache::setExecutablePath);
-    }
-
     NimSettings settings;
     NimBuildConfigurationFactory buildConfigFactory;
     NimbleBuildConfigurationFactory nimbleBuildConfigFactory;
@@ -91,7 +80,6 @@ public:
     NimCompilerBuildStepFactory buildStepFactory;
     NimCompilerCleanStepFactory cleanStepFactory;
     NimCodeStyleSettingsPage codeStyleSettingsPage;
-    NimToolsSettingsPage toolsSettingsPage;
     NimCodeStylePreferencesFactory codeStylePreferencesPage;
     NimToolChainFactory toolChainFactory;
 };
