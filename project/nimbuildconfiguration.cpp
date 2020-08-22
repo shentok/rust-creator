3821022afc364aae5412bb315d0e325cdedcb52a
Bernhard Beschow <shentey@gmail.com>
2020-08-22 22:01:01 +0200

    Factor out build configuration widget

--- project/nimbuildconfiguration.cpp
+++ project/nimbuildconfiguration.cpp
@@ -24,6 +24,8 @@
 ****************************************************************************/
 
 #include "nimbuildconfiguration.h"
+
+#include "nimbuildconfigurationwidget.h"
 #include "nimcompilerbuildstep.h"
 #include "nimproject.h"
 
@@ -86,21 +88,20 @@ NimBuildConfiguration::NimBuildConfiguration(Target *target, Utils::Id id)
         QTC_ASSERT(buildStep, return);
         auto cleanStep = nimCompilerCleanStep();
         QTC_ASSERT(cleanStep, return);
-        NimCompilerBuildStep::DefaultBuildOptions defaultOption;
+        DefaultBuildOptions defaultOption;
         switch (info.buildType) {
         case BuildConfiguration::Release:
-            defaultOption = NimCompilerBuildStep::DefaultBuildOptions::Release;
+            defaultOption = DefaultBuildOptions::Release;
             break;
         case BuildConfiguration::Debug:
-            defaultOption = NimCompilerBuildStep::DefaultBuildOptions::Debug;
+            defaultOption = DefaultBuildOptions::Debug;
             break;
         default:
-            defaultOption = NimCompilerBuildStep::DefaultBuildOptions::Empty;
+            defaultOption = DefaultBuildOptions::Empty;
             break;
         }
-        buildStep->setDefaultCompilerOptions(defaultOption);
+        setDefaultCompilerOptions(defaultOption);
         buildStep->setUserCompilerOptions(QStringList() << "build");
-        cleanStep->setDefaultCompilerOptions(defaultOption);
         cleanStep->setUserCompilerOptions(QStringList() << "clean");
 
         const Utils::FilePaths nimFiles = project()->files([](const Node *n) {
@@ -108,19 +109,72 @@ NimBuildConfiguration::NimBuildConfiguration(Target *target, Utils::Id id)
         });
 
         if (!nimFiles.isEmpty()) {
-            buildStep->setTargetNimFile(nimFiles.first());
-            cleanStep->setTargetNimFile(nimFiles.first());
+            setTargetNimFile(nimFiles.first());
         }
     });
+
+    connect(project(), &ProjectExplorer::Project::fileListChanged,
+            this, &NimBuildConfiguration::updateTargetNimFile);
+    connect(this, &NimBuildConfiguration::targetNimFileChanged,
+            this, &NimBuildConfiguration::processParametersChanged);
+    connect(this, &NimBuildConfiguration::defaultCompilerOptionsChanged,
+            this, &NimBuildConfiguration::processParametersChanged);
+}
+
+
+NamedWidget *NimBuildConfiguration::createConfigWidget()
+{
+    return new NimBuildConfigurationWidget(this);
 }
 
 
 bool NimBuildConfiguration::fromMap(const QVariantMap &map)
 {
+    m_defaultOptions = static_cast<DefaultBuildOptions>(map[Constants::C_NIMCOMPILERBUILDSTEP_DEFAULTBUILDOPTIONS].toInt());
+    m_targetNimFile = FilePath::fromString(map[Constants::C_NIMCOMPILERBUILDSTEP_TARGETNIMFILE].toString());
+
     if (!ProjectExplorer::ProjectConfiguration::fromMap(map))
         return false;
 
     return ProjectExplorer::BuildConfiguration::fromMap(map);
+}
+
+QVariantMap NimBuildConfiguration::toMap() const
+{
+    QVariantMap result = BuildConfiguration::toMap();
+    result[Constants::C_NIMCOMPILERBUILDSTEP_DEFAULTBUILDOPTIONS] = m_defaultOptions;
+    result[Constants::C_NIMCOMPILERBUILDSTEP_TARGETNIMFILE] = m_targetNimFile.toString();
+    return result;
+}
+
+
+NimBuildConfiguration::DefaultBuildOptions NimBuildConfiguration::defaultCompilerOptions() const
+{
+    return m_defaultOptions;
+}
+
+
+void NimBuildConfiguration::setDefaultCompilerOptions(const DefaultBuildOptions options)
+{
+    if (m_defaultOptions == options)
+        return;
+    m_defaultOptions = options;
+    emit defaultCompilerOptionsChanged(options);
+}
+
+
+FilePath NimBuildConfiguration::targetNimFile() const
+{
+    return m_targetNimFile;
+}
+
+
+void NimBuildConfiguration::setTargetNimFile(const FilePath &targetNimFile)
+{
+    if (targetNimFile == m_targetNimFile)
+        return;
+    m_targetNimFile = targetNimFile;
+    emit targetNimFileChanged(targetNimFile);
 }
 
 
@@ -128,8 +182,19 @@ FilePath NimBuildConfiguration::outFilePath() const
 {
     const NimCompilerBuildStep *step = nimCompilerBuildStep();
     QTC_ASSERT(step, return FilePath());
-    const QString targetName = Utils::HostOsInfo::withExecutableSuffix(step->targetNimFile().toFileInfo().baseName());
+    const QString targetName = Utils::HostOsInfo::withExecutableSuffix(m_targetNimFile.toFileInfo().baseName());
     return buildDirectory().pathAppended(targetName);
+}
+
+void NimBuildConfiguration::updateTargetNimFile()
+{
+    if (!m_targetNimFile.isEmpty())
+        return;
+    const Utils::FilePaths nimFiles = project()->files([](const Node *n) {
+        return Project::AllFiles(n) && n->path().endsWith(".toml");
+    });
+    if (!nimFiles.isEmpty())
+        setTargetNimFile(nimFiles.at(0));
 }
 
 const NimCompilerBuildStep *NimBuildConfiguration::nimCompilerBuildStep() const
